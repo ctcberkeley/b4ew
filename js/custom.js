80d4760ca0e616de5d7eb45b6073729818bff73d
Mari Batilando <mari.batilando@gmail.com>
2014-02-05 20:44:57 -0800

    Finish officers page

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
+  $('.flip').hover(
+       function(){ $(this).find('.card').addClass('flipped') },
+       function(){ $(this).find('.card').removeClass('flipped') }
+	)
 });
